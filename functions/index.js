93e01cb8395b144fc905b433089d021a000171eb
MikeSubbotsky <submix75@gmail.com>
2023-03-30 00:09:47 +0300

    openai error handling

--- functions/index.js
+++ functions/index.js
@@ -2,24 +2,24 @@ const express = require("express");
 const functions = require("firebase-functions");
 const path = require("path");
 const dotenv = require("dotenv");
-//const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const chatRoutes = require("./routes/chat");
 
 dotenv.config();
 
 const app = express();
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(cors());
 
 // Serve the React build files
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../client/build")));
   
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-    });
-  }
+//     app.get("*", (req, res) => {
+//       res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+//     });
+//   }
   
 app.use("/api/chat", chatRoutes);
 
